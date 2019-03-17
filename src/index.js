5998a8d769efeba3ff5290a49043084a3460cbd6
MRashchynskaya <m.redqueen@gmail.com>
2019-03-17 22:41:44 +0300

    feat: find correct exchange

--- src/index.js
+++ src/index.js
@@ -2,4 +2,29 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    let summ = {};
+    const coins = { 
+        H: 50, 
+        Q: 25, 
+        D: 10, 
+        N: 5, 
+        P: 1 
+    };
+    const maxSumm = 10000;
+
+    if (currency <= maxSumm) {
+        let currentSumm = currency;
+        for (let key in coins) {
+            const coinsAmount = Math.floor(currentSumm / coins[key]);
+            if (coinsAmount > 0) {
+                summ[key] = coinsAmount;
+                currentSumm = currentSumm - (coinsAmount * coins[key]);
+            }
+        }
+    }
+    else {
+        summ.error = "You are rich, my friend! We don't have so much coins for exchange";
+    }
+
+    return summ;
 }
